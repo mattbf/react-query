42cece395d90dacc300b69f62b5e893eea8b87d8
MatthewBF <matthewbf8@gmail.com>
2024-02-08 16:02:40 -0500

    added label filtering

--- next-app/app/issues/page.tsx
+++ next-app/app/issues/page.tsx
@@ -1,16 +1,30 @@
+"use client";
 import IssuesList from "@/components/IssuesList";
 import LabelList from "@/components/LabelList";
+import { useState } from "react";
 
 const Issues = () => {
+  const [labels, setLabels] = useState<string[]>([]);
+
+  const handleSelectLabel = (label: string) => {
+    setLabels((currentLabels) => {
+      if (currentLabels.includes(label)) {
+        return currentLabels.filter((l) => l !== label);
+      } else {
+        return currentLabels.concat(label);
+      }
+    });
+  };
+
   return (
     <div>
       <main>
         <section>
           <h1>Issues</h1>
-          <IssuesList />
+          <IssuesList labels={labels} />
         </section>
         <aside>
-          <LabelList />
+          <LabelList selected={labels} toggle={(label) => handleSelectLabel(label)} />
         </aside>
       </main>
     </div>
--- next-app/components/IssuesList.tsx
+++ next-app/components/IssuesList.tsx
@@ -2,8 +2,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { IssueItem } from "@/components/IssueItem";
 
-export default function IssuesList() {
-  const issuesQuery = useQuery({ queryKey: ["issues"], queryFn: () => fetch("/api/issues").then((res) => res.json()) });
+export default function IssuesList({ labels }: { labels: string[] }) {
+  const issuesQuery = useQuery({
+    queryKey: ["issues", { labels }],
+    queryFn: () => {
+      const labelsString = labels.map((l) => `labels[]=${l}`).join("&");
+      fetch(`/api/issues?${labelsString}`).then((res) => res.json());
+    },
+  });
+
   return (
     <div>
       <h2>Issues List</h2>
@@ -11,7 +18,7 @@ export default function IssuesList() {
         <p>Loading....</p>
       ) : !!issuesQuery.data && issuesQuery.isSuccess ? (
         <ul className="issues-list">
-          {issuesQuery.data.map((item) => (
+          {issuesQuery?.data?.map((item) => (
             <IssueItem
               key={item.id}
               title={item.title}
